bbd22d515c5e32e0560d3a0198ad985b656c4820
Sakran21 <moham.alsakran@gmail.com>
2026-07-21 23:32:15 +0300

    week1 monday environment verification

---
+++ src/Week1/Monday/VerifyApp.java
@@ -0,0 +1,9 @@
+package Week1.Monday;
+
+public class VerifyApp {
+    public static void main(String[] args) {
+        System.out.println("=== Environment Verification successful! ===");
+        System.out.println("JDK Version: " + System.getProperty("java.version"));
+        System.out.println("OS Name:     " + System.getProperty("os.name"));
+    }
+}
